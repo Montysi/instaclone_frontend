0e88a972d33331081b9e93944ec893f1ae360873
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-07-22 13:56:31 +0100

    chore: setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>{import.meta.env.VITE_COOL_TEST}</h1>
+    </>
+  );
+}
+
+export default App
